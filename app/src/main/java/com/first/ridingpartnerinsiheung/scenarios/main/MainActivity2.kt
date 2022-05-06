b52647b9d240c97b0f9947ef2048a8b61d2045c9
bjk1649 <qhwns1649@naver.com>
2022-05-06 14:44:28 +0900

    feat : "버튼 누르면 다른 액티비티로 이동"

---
+++ app/src/main/java/com/first/ridingpartnerinsiheung/scenarios/main/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.first.ridingpartnerinsiheung.scenarios.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.first.ridingpartnerinsiheung.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
