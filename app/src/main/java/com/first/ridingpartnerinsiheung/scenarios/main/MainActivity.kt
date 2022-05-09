837b6da43d34dbc61debc264c6282765ef61591f
hyejinxx <haejinjung1110@gmail.com>
2022-05-09 13:34:13 +0900

    add : 뷰바인딩, 뷰모델 선언 추가

--- app/src/main/java/com/first/ridingpartnerinsiheung/scenarios/main/MainActivity.kt
+++ app/src/main/java/com/first/ridingpartnerinsiheung/scenarios/main/MainActivity.kt
@@ -3,7 +3,10 @@ package com.first.ridingpartnerinsiheung.scenarios.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.first.ridingpartnerinsiheung.R
+import com.first.ridingpartnerinsiheung.databinding.ActivityMainBinding
 import com.first.ridingpartnerinsiheung.scenarios.main.fragment.DisplayMapFragment
 import com.first.ridingpartnerinsiheung.scenarios.main.fragment.ListViewFragment
 
@@ -11,6 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     var lgt: Int = 0
     var rtt : Int = 0
+
+    val viewModel by viewModels<MainViewModel>()
+    val binding by lazy { DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,8 +56,5 @@ class MainActivity : AppCompatActivity() {
                 ft.replace(R.id.mainFrame, DisplayMapFragment()).commit()
             }
         }
-
-
-
     }
 }
