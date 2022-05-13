263556021c2fc4376a4a37544a6f7275ff54f9e0
itaebro <plus100kt@gmail.com>
2022-05-13 11:51:11 +0900

    feat: 마이페이지 추가

---
+++ app/src/main/java/com/first/ridingpartnerinsiheung/data/Record.kt
@@ -0,0 +1,5 @@
+package com.first.ridingpartnerinsiheung.data
+
+class Record (val id : String, val name : String, val content : String, val img: String) {
+    var record = arrayListOf<Record>()
+}
