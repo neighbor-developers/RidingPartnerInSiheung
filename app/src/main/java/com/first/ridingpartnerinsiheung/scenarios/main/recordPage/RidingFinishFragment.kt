6c4a17659a6d0270500577b24e128c1f44916c5b
SeoYeonn05 <seoyeonn051@gmail.com>
2022-06-11 15:10:30 +0900

    fix : 사진 firebase에 저장

--- app/src/main/java/com/first/ridingpartnerinsiheung/scenarios/main/recordPage/RidingFinishFragment.kt
+++ app/src/main/java/com/first/ridingpartnerinsiheung/scenarios/main/recordPage/RidingFinishFragment.kt
@@ -1,14 +1,26 @@
 package com.first.ridingpartnerinsiheung.scenarios.main.recordPage
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewModelScope
+import com.bumptech.glide.Glide
 import com.first.ridingpartnerinsiheung.R
 import com.first.ridingpartnerinsiheung.data.RidingData
 import com.first.ridingpartnerinsiheung.databinding.RidingFinishFragmentBinding
@@ -18,6 +30,12 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class RidingFinishFragment : Fragment() {
 
@@ -28,6 +46,15 @@ class RidingFinishFragment : Fragment() {
     private var data : RidingData? = null
 
     private var time : String? = null
+    private val REQUEST_CODE:Int = 99
+    private var imageUri :Uri? = null
+
+    // Firebase
+    private val auth = Firebase.auth
+    private val user = auth.currentUser!!.uid
+    private var db = FirebaseStorage.getInstance()
+    private val storageRef = db.reference
+    var ridingContent = MutableStateFlow("")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +66,7 @@ class RidingFinishFragment : Fragment() {
 
         initBinding()
         initTextChanged()
+        initClickListener()
         finishClickListener()
         initData(time!!, data!!)
 
@@ -52,6 +80,7 @@ class RidingFinishFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
     }
+
     private fun finishClickListener() {
         binding.complete.setOnClickListener {
             val recordActivity = activity as RecordActivity
@@ -64,11 +93,125 @@ class RidingFinishFragment : Fragment() {
             memo = it.toString()
         }
     }
+
     private fun initData(time : String, data: RidingData){
         viewModel.savedTimer.value = data.timer
         viewModel.savedSpeed.value = data.averSpeed
         viewModel.savedKcal.value = data.kcal
         viewModel.savedDistance.value = data.sumDistance
         viewModel.savedTime.value = time
+    }
+
+    // 권한 부여
+    private fun requestPermission(): Boolean {
+        val writePermission: Int = ContextCompat.checkSelfPermission(requireContext(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        val readPermission: Int = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        if (writePermission == PackageManager.PERMISSION_GRANTED && readPermission == PackageManager.PERMISSION_GRANTED) {
+            return true
+        } else (return false)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty()) {
+                    var isAllGranted = true
+                    for (grant in grantResults) {
+                        if (grant != PackageManager.PERMISSION_GRANTED) { //권한 거부
+                            isAllGranted = false
+                            break
+                        }
+                        if (isAllGranted) {
+                        } //권한 동의
+                        else { //권한 거부했을경우
+                            if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                                    requireActivity(),
+                                    Manifest.permission.READ_EXTERNAL_STORAGE
+                                ) ||
+                                !ActivityCompat.shouldShowRequestPermissionRationale(
+                                    requireActivity(),
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                                )
+                            ) {
+                                requestPermission()
+                            } else {
+                                break
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private var getContent: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            lateinit var _imageUri : Uri
+            result.data?.data?.let {
+                _imageUri = it
+                Glide.with(this)
+                    .load(it)
+                    .into(binding.cImage)
+                binding.cImage.clipToOutline
+            }
+            imageUri = _imageUri
+        }
+    }
+
+    private fun setImage(){
+        if (requestPermission()) {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+            intent.type = "image/*"
+            getContent.launch(intent)
+        } else {
+            ActivityCompat.requestPermissions(
+                (requireActivity()), arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                REQUEST_CODE
+            )
+        }
+    }
+    private fun initClickListener() {
+        // 갤러리에서 이미지 불러오기
+        binding.cImage.setOnClickListener {
+            setImage()
+        }
+    }
+
+    private fun saveData(){
+        addDiaryContent(
+            onSuccess = {showToast("저장")},
+            onFailure = {showToast("실패")})
+        addResultImage()
+    }
+
+
+    private fun addResultImage(){
+        val fileName = user + viewModel.savedTime.value.toString() +".png"
+        imageUri?.let {
+            storageRef.child(user).child(fileName).putFile(it)
+                .addOnSuccessListener {
+                    /*no-op*/
+                }
+                .addOnFailureListener{
+                    showToast("사진 저장에 실패하였습니다.")
+                }
+        }
     }
 }
